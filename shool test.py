ea584e433b63c236dcadceb1029012daf11bc442
yohanna-dev <uhanna13915334001391@gmail.com>
2024-08-12 12:10:44 +0330

    Update shool test.py

--- shool test.py
+++ shool test.py
@@ -1,1 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
 
+class SimpleApp(App):
+    def build(self):
+        return Label(text='سلام')
+
+if name == '__main__':
+    SimpleApp().run()
